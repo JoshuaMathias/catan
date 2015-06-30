a1f820e0621c83b6a893b5bd615a5d1a9468db62
Daniel Stefan Ribble <dsribble@knife.cs.byu.edu>
2015-06-30 16:55:31 -0600

    Client Model stuff

--- client/model/ClientModel.java
+++ client/model/ClientModel.java
@@ -1,5 +1,17 @@
 package client.model;
 
-public class ClientModel {
+import java.util.ArrayList;
 
+public class ClientModel {
+	private ResourceList bank;
+	private MessageList chat;
+	private MessageList log;
+	private Map map;
+	private ArrayList<Player> players;
+	private TradeOffer tradeOffer;
+	private TurnTracker turntracker;
+	private int version;
+	private int winner = -1;
+	
+	
 }
