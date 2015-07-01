28cfb45cf8fd6c353ea1fbf366e2214c921712f4
Daniel Stefan Ribble <dsribble@knife.cs.byu.edu>
2015-07-01 09:03:12 -0600

    Client Model Comments, but didn't finish

--- client/model/ClientModel.java
+++ client/model/ClientModel.java
@@ -2,6 +2,11 @@ package client.model;
 
 import java.util.ArrayList;
 
+/**
+ * Class represents model of game on client side
+ * @author Ife's Group
+ *
+ */
 public class ClientModel {
 	private ResourceList bank;
 	private MessageList chat;
@@ -13,103 +18,214 @@ public class ClientModel {
 	private int version;
 	private int winner = -1;
 	
-	private Map initializeMap(ClientModel clientModel) {
+	/**
+	 * initializes map and resets map when needed
+	 * @param clientModel
+	 * @pre clientModel != null
+	 * @post Map is updated
+	 */
+	public Map initializeMap(ClientModel clientModel) {
 		
 		Map map = null;
 		
 		return map;
 	}
 	
-	private int rollDice() {
+	/**
+	 * rolls dice for current player
+	 * @pre is current player's turn
+	 * @post player is given an int representing value of dice roll
+	 */
+	public int rollDice() {
 		
 		int diceRoll = 7;
 		
 		return diceRoll;
 	}
 	
-	private void finishTurn() {
+	/**
+	 * Ends current Player's turn
+	 * @pre is a player's turn
+	 * @post is next player's turn
+	 */
+	public void finishTurn() {
 		
 	}
 	
-	private void bankTrade() {
+	/**
+	 * trades a given number of one type of resource card with game card bank
+	 * @pre is players turn and player has required number of one type of resource card
+	 * @post player has required number less of one resource card and one more of another type
+	 */
+	public void bankTrade() {
 		
 	}
 	
-	private void offerTrade() {
+	/**
+	 * sends trade offer to another player
+	 * @pre trade offer is held by player
+	 * @post message is sent to other player
+	 */
+	public void offerTrade() {
 		
 	}
 	
-	private void acceptTrade() {
+	/**
+	 * accepts trade offer from other player and adjusts card number
+	 * @pre player has required amount of resources in offer
+	 * @post player either accepts and changes amount of resource card number or rejects trade offer
+	 */
+	public void acceptTrade() {
 		
 	}
 	
-	private void buyDevCard() {
+	/**
+	 * player purchases development card from game card bank for required resource card amount
+	 * @pre player has required resource card amount and is player's turn
+	 * @post player recieves a development card and pays resource cards
+	 */
+	public void buyDevCard() {
 		
 	}
 	
-	private void buildSettlement() {
+	/**
+	 * player builds a settlement on map
+	 * @pre player has enough resource cards and spot on map is valid
+	 * @post Settlement is built and resources are used
+	 */
+	public void buildSettlement() {
 		
 	}
 	
-	private void buildCity() {
+	/**
+	 * player builds a city in place of a settlement
+	 * @pre player has enough resource cards and spot on map has settlement
+	 * @post settlement is replaced by city and resources are used
+	 */
+	public void buildCity() {
 		
 	}
 	
-	private void buildRoad() {
+	/**
+	 * player builds road on map
+	 * @pre player has enough resource cards and edge location on map is valid
+	 * @post Road is built and resources are used
+	 */
+	public void buildRoad() {
 		
 	}
 	
-	private void playYearOfPlenty() {
+	/**
+	 * player plays year of plenty card
+	 * @pre is player's turn, player hasn't played a development card during current turn, and player has year of plenty card in old dev cards pile
+	 * @post effect is applied and year of plenty card is discarded
+	 */
+	public void playYearOfPlenty() {
 		
 	}
 	
-	private void playRoadBuilderCard() {
+	/**
+	 * player playes road builder card
+	 * @pre is player's turn, player hasn't played a development card during current turn and player has road builder card in old dev cards pile
+	 * @post effect is applied and road builder card is discarded
+	 */
+	public void playRoadBuilderCard() {
 		
 	}
 	
-	private void playSoldierCard() {
+	/**
+	 * player plays soldier card and moves the thief
+	 * @pre is player's turn, player hasn't played a development card during current turn, and player has soldier card in old dev cards pile
+	 * @post effect is applied and soldier card is discarded
+	 */
+	public void playSoldierCard() {
 		
 	}
 	
-	private void playMonopolyCard() {
+	/**
+	 * player plays monopoly card
+	 * @pre is player's turn, player hasn't played a development card during current turn, and player has monopoly card in old dev cards pile
+	 * @post effect is applied and monopoly card is discarded
+	 */
+	public void playMonopolyCard() {
 		
 	}
 	
-	private void playVictoryCard() {
+	/**
+	 * player plays victory card and gains a victory point
+	 * @pre is player's turn, player hasn't played a development card during current turn, and player has victory card in old dev cards pile
+	 * @post victory point is gained and victory card is discarded
+	 */
+	public void playVictoryCard() {
 		
 	}
 	
-	private void buyResourceCard() {
+	/**
+	 * player purchases resource card
+	 * @pre
+	 * @post
+	 */
+	public void buyResourceCard() {
 		
 	}
 	
-	private void discardHalfCards() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public void discardHalfCards() {
 		
 	}
 	
-	private void moveRobber() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public void moveRobber() {
 		
 	}
 	
-	private void stealResourceCard(Player player) {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public void stealResourceCard(Player player) {
 		
 	}
 	
-	private int checkLongestRoad() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public int checkLongestRoad() {
 		
 		int longestRoadCount = 0;
 		
 		return longestRoadCount;
 	}
 	
-	private int checkLargestArmy() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public int checkLargestArmy() {
 		
 		int longestArmyCount = 0;
 		
 		return longestArmyCount;
 	}
 	
-	private int checkVictoryPoints() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public int checkVictoryPoints() {
 		
 		int victoryPointCount = 0;
 		
@@ -118,91 +234,166 @@ public class ClientModel {
 	
 	//Can Methods Below********************************************************************************
 	
-	private boolean canRollDice() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canRollDice() {
 		
 		boolean test = false;
 		return test;
 	}
 
-	private boolean canbankTrade() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canbankTrade() {
 		
 		boolean test = false;
 		return test;
 	}
 	
-	private boolean canOfferTrade() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canOfferTrade() {
 	
 		boolean test = false;
 		return test;
     }
 
-	private boolean canAcceptTrade() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canAcceptTrade() {
 	
 		boolean test = false;
 		return test;
 	}
 
-	private boolean canBuyDevCard() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canBuyDevCard() {
 	
 		boolean test = false;
 		return test;
 	}
 
-	private boolean canbuildSettlement() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canbuildSettlement() {
 	
 		boolean test = false;
 		return test;
 	}
 
-	private boolean canBuildSettlement() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canBuildSettlement() {
 	
 		boolean test = false;
 		return test;
 	}
 	
-	private boolean canBuildCity() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canBuildCity() {
 	
 		boolean test = false;
 		return test;
 	}
 	
-	private boolean canBuildRoad() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canBuildRoad() {
 	
 		boolean test = false;
 		return test;
 	}
 
-	private boolean canPlayDevCard() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canPlayDevCard() {
 	
 		boolean test = false;
 		return test;
 	}
 	
-	private boolean canBuyResourceCard() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canBuyResourceCard() {
 		
 		boolean test = false;
 		return test;
 	}
 	
-	private boolean canPlaceRobber() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canPlaceRobber() {
 	
 		boolean test = false;
 		return test;
 	}
 	
-	private boolean canDiscardHalfCards() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canDiscardHalfCards() {
 	
 		boolean test = false;
 		return test;
 	}
 
-	private boolean canStealResourceCard() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean canStealResourceCard() {
 	
 		boolean test = false;
 		return test;
 	}
 	
-	private boolean mustDiscardHalfCards() {
+	/**
+	 * 
+	 * @pre
+	 * @post
+	 */
+	public boolean mustDiscardHalfCards() {
 		boolean test = false;
 		return test;
 	}
