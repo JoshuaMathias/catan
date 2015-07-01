1815fbcde09fd44933577900697f10948031bccb
Daniel Stefan Ribble <dsribble@knife.cs.byu.edu>
2015-07-01 13:06:01 -0600

    Client Model

--- client/serverproxy/ClientCommunicator.java
+++ client/serverproxy/ClientCommunicator.java
@@ -9,6 +9,13 @@ import client.model.ClientModel;
  */
 public class ClientCommunicator {
 	
+	/**
+	 * packages information from ServerProxy and sends to Server
+	 * @param command
+	 * @param params
+	 * @pre command is a valid command that the Server would understand. Params must not be null and all can do methods applying to the command in the client model have returned true
+	 * @post sends the command and params to the server
+	 */
 	public ClientModel send(String command, Object params){
 		return null;
 		
