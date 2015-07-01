d7acfe26300f691f72f73dc37260c708343752d8
Daniel Stefan Ribble <dsribble@knife.cs.byu.edu>
2015-07-01 12:56:36 -0600

    Client Model

---
+++ client/serverproxy/ClientCommunicator.java
@@ -0,0 +1,16 @@
+package client.serverproxy;
+
+import client.model.ClientModel;
+
+/**
+ * Packages and sends commands to the Server from the ServerProxy
+ * @author Ife's Group
+ *
+ */
+public class ClientCommunicator {
+	
+	public ClientModel send(String command, Object params){
+		return null;
+		
+	}
+}
