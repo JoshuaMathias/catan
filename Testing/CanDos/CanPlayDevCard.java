a1e809d31a964219ddaaf39f84468544e4e67f2b
Paul Randal Cloward <randalpp@halberd.cs.byu.edu>
2015-07-08 12:12:21 -0600

    finished cando junit canplaydevcard

--- Testing/CanDos/CanPlayDevCard.java
+++ Testing/CanDos/CanPlayDevCard.java
@@ -75,11 +75,25 @@ public class CanPlayDevCard {
 		
 		turnTracker.setCurrentTurn(0);
 		turnTracker.setStatus("Playing");
-		Ife.setPlayedDevCard(true);
+		Ife.setPlayedDevCard(false);
 		DevCardList ifeDevCardList = new DevCardList(2,2,1,1,0);
 		Ife.setOldDevCards(ifeDevCardList);
 		
-		assertFalse(clientModel.canPlayDevCard(0, DevCardType.MONOPOLY));
+		assertTrue(clientModel.canPlayDevCard(0, DevCardType.MONOPOLY));//Ife has everything he needs to play
+		Ife.setPlayedDevCard(true);
+		assertFalse(clientModel.canPlayDevCard(0, DevCardType.MONOPOLY));//Ife should be able to play Monopoly card. Its his turn and playing status. But Ife has already played a Dev card this turn
+	
+		assertFalse(clientModel.canPlayDevCard(0, DevCardType.YEAR_OF_PLENTY));//Ife doesnt have a Year Of Plenty Card but tries to play it
+	
+		Josh.setPlayedDevCard(false);
+		DevCardList joshDevCardList = new DevCardList(1,1,1,1,1);
+		assertFalse(clientModel.canPlayDevCard(1, DevCardType.MONUMENT));//Josh fails because it is not his turn
+		
+		turnTracker.setStatus("Robbing");
+		Ife.setPlayedDevCard(false);
+		assertFalse(clientModel.canPlayDevCard(0, DevCardType.MONUMENT));//The status is not set to playing. Ife can not play dev card
+		
+		
 		
 	}
 }
