cf3e3889ab944798fd09e222b71b7b099aaab54d
Daniel Stefan Ribble <dsribble@katana.cs.byu.edu>
2015-07-08 15:27:28 -0600

    fixed CanBuildSettlement

--- client/model/ClientModel.java
+++ client/model/ClientModel.java
@@ -443,7 +443,6 @@ public class ClientModel {
 		int whoseTurn = turnTracker.getCurrentTurn();
 		String status = turnTracker.getStatus();
 		
-		boolean can = false;
 		
 		if (playerIndex == whoseTurn && status.equals("Playing")){
 			ResourceList playersResources = players.get(playerIndex).getResources();
@@ -472,13 +471,15 @@ public class ClientModel {
 			if (map.isSpotTaken(settlementSpot)){
 				return false;
 			}
+			else if (map.isSpotNeighbored(settlementSpot)){
+				return false;
+			}
 			else{
 				return true;
 			}
 		}
 		
-		
-		return can;
+		return false;
 	}
 	
 	/**
