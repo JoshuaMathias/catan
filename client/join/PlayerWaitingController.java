a978acdd209277d739a2fbd4a0429c550ed03730
Joshua Mathias <joshuamonkey@gmail.com>
2015-07-13 17:02:22 -0600

    Implementing player waiting controller

--- client/join/PlayerWaitingController.java
+++ client/join/PlayerWaitingController.java
@@ -1,6 +1,11 @@
 package client.join;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import client.base.*;
+import client.data.PlayerInfo;
+import client.facade.Facade;
 
 
 /**
@@ -8,20 +13,23 @@ import client.base.*;
  */
 public class PlayerWaitingController extends Controller implements IPlayerWaitingController {
 
+	private Facade clientFacade;
+	
 	public PlayerWaitingController(IPlayerWaitingView view) {
-
 		super(view);
+		clientFacade=Facade.getSingleton();
 	}
 
 	@Override
 	public IPlayerWaitingView getView() {
-
 		return (IPlayerWaitingView)super.getView();
 	}
 
 	@Override
 	public void start() {
-		
+		List<PlayerInfo> playersList = clientFacade.gamesList().getGames().get(clientFacade.getCurrentGameId()).getPlayers();
+		PlayerInfo[] players = new PlayerInfo[playersList.size()];
+		getView().setPlayers(players);
 		getView().showModal();
 	}
 
