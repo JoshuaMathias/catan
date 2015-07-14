6d73a81c023f8c2d9b2bca8c1b97f331c13673f6
Paul Randal Cloward <randalpp@foil.cs.byu.edu>
2015-07-13 22:40:09 -0600

    gamehistorycontroller now working. havent checked for bugs

--- client/communication/GameHistoryController.java
+++ client/communication/GameHistoryController.java
@@ -2,10 +2,10 @@ package client.communication;
 
 import java.util.*;
 
+
 import client.base.*;
 import client.facade.Facade;
 import client.model.ClientModel;
-import client.model.Map;
 import client.model.MessageLine;
 import client.model.MessageList;
 import client.model.Player;
@@ -42,16 +42,23 @@ public class GameHistoryController extends Controller implements IGameHistoryCon
 		
 		MessageList messages = clientModel.getLog();
 		ArrayList<MessageLine> lines = messages.getLines();
+		System.out.println("Line Size: " + lines.size());
+		
+		if(lines.size() == 1) {
+			System.out.println(lines.get(0).getMessage());
+		}
 		
 		for(MessageLine line: lines) {
 			
 			for(Player player : players) {
-
-				if(player.equals(line.getSource())) {
+				
+				
+				if(player != null && player.getName().equals(line.getSource())) {
 					
 					String message = line.getMessage();
 					CatanColor color = player.getColor();
 					entries.add(new LogEntry(color, message));
+					System.out.println("IN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1");
 				}
 			}
 		}
