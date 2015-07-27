ed5552de39391d86a581dae54602105998e59133
Daniel Stefan Ribble <dsribble@knife.cs.byu.edu>
2015-07-27 16:23:48 -0600

    begging of Server Phase 3

--- server/facade/ServerFacade.java
+++ server/facade/ServerFacade.java
@@ -15,90 +15,156 @@ public class ServerFacade {
 		return serverFacade;
 	}
 // All methods return void only as placeholders. They should all return specific response types	
+	/**
+	 * 
+	 */
 	public void acceptTrade(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void buildCity(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void buildRoad(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void buildSettlement(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void buyDevCard(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void createGame(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void discardCards(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void finishTurn(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void GameModel(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void GamesList(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void joinGame(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void logIn(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void maritimeTrade(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void monument(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void offerTrade(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void register(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void roadBuilding(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void robPlayer(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void rollNumber(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void sendChat(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void soldier(){
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void yearOfPlenty(){
 		
 	}
