8fd93a576b0bc57d3fd563d9868550e8c9857d4f
Paul Randal Cloward <randalpp@longbow.cs.byu.edu>
2015-08-03 12:58:57 -0600

    JUnit test for DiscardCardsCommand

--- testing/command/DiscardCardsCommand.java
+++ testing/command/DiscardCardsCommand.java
@@ -22,6 +22,7 @@ public class DiscardCardsCommand {
 	private Player ife = new Player();
 	private Player josh = new Player();
 	private GameModel game;
+	private TurnTracker turnTracker;
 	
 	@Before 
 	public void setUp() {
@@ -42,10 +43,31 @@ public class DiscardCardsCommand {
 		players.add(ife);
 		players.add(josh);
 		
+		paul.setName("paul");
+		daniel.setName("daniel");
+		ife.setName("ife");
+		josh.setName("josh");
+		
+		daniel.setDiscarded(false);
+		ife.setDiscarded(false);
+		josh.setDiscarded(false);
+		
+		ResourceList paulsResources = new ResourceList(9,9,9,9,9);
+		paul.setResources(paulsResources);
+		
+		ResourceList danielsResources = new ResourceList(1,1,1,1,1);
+		daniel.setResources(danielsResources);
+		
+		ResourceList ifesResources = new ResourceList(2,2,2,2,2);
+		ife.setResources(ifesResources);
+		
+		ResourceList joshsResources = new ResourceList(2,2,2,2,2);
+		josh.setResources(joshsResources);
+		
 		game.setPlayers(players);
 		
-		TurnTracker turnTracker = new TurnTracker();
-		turnTracker.setStatus("Playing");
+		turnTracker = new TurnTracker();
+		turnTracker.setStatus("Discarding");
 		turnTracker.setCurrentTurn(0);
 		game.setTurnTracker(turnTracker);
 		serverFacade.addGameToList(game);
@@ -61,9 +83,26 @@ public class DiscardCardsCommand {
 	public void test() {
 		
 		ResourceList discardResourceList = new ResourceList(4,4,4,4,4);
-		paul.setResources(discardResourceList);
 		serverFacade.discardCards(0, discardResourceList, 0);
-		System.out.println(paul.getResources().getTotal());
+		
+		assertEquals(25,paul.getResources().getTotal());//Should take away 20 resources from pauls 45 resources leaving behind 20 left
+		
+		turnTracker.setCurrentTurn(1);
+		turnTracker.setStatus("Discarding");
+		discardResourceList = new ResourceList(1,1,1,0,0);
+		serverFacade.discardCards(1, discardResourceList, 0);
+		assertEquals(5,daniel.getResources().getTotal());//Daniel doesnt have enough resources to discard. So should stay at 5 resources
+		
+		turnTracker.setCurrentTurn(3);
+		turnTracker.setStatus("Discarding");
+		discardResourceList = new ResourceList(1,1,1,1,1);
+		serverFacade.discardCards(3, discardResourceList, 0);
+		assertEquals(5,josh.getResources().getTotal());//Half of Joshs 10 resources were discarded in half 
+		
+		turnTracker.setCurrentTurn(2);
+		discardResourceList = new ResourceList(1,1,1,1,1);
+		serverFacade.discardCards(2, discardResourceList, 0);
+		assertEquals(10,ife.getResources().getTotal());//The turnTracker is in the robbing status. Should not discard ifes cards
 	}
 
 }
